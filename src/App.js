5dccffde32271fab4ca59c7a158cc052a62c4a6f
Jimen Woo <jimenwoo@Jimens-MacBook-Pro.local>
2019-04-30 16:40:29 -0700

    delete todos

--- src/App.js
+++ src/App.js
@@ -7,19 +7,43 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state ={
+      delete: "",
       submit: "",
-      toDo : []
+      toDo : [],
+      checked: false
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   handleChange(event) {
     this.setState({
       submit : event.target.value
     })
-    }
+  }
 
+  handleCheck(event){
+    let arr = this.state.toDo.filter((item) => {
+      return item != event.target.value
+    })
+    this.setState({
+      delete: event.target.value,
+      toDo : arr
+    })
+  }
+
+  handleKeyDown(e){
+    if(e.key == "Enter") {
+      var newArray = this.state.toDo.slice();
+      newArray.push(this.state.submit);
+      this.setState({
+        submit: "",
+        toDo : newArray
+      })  
+    }
+  }
   handleClick(){
     var newArray = this.state.toDo.slice();
     newArray.push(this.state.submit);
@@ -32,10 +56,10 @@ class App extends Component {
   render() {
     return (
       <div className = "wrapper">
-      <input onChange = {this.handleChange} id="textField" value = {this.state.submit} type="text" class="form-control" placeholder="Enter new Task"/>
+      <input onChange = {this.handleChange} autofocus="autofocus" id="textField" value = {this.state.submit} type="text" class="form-control" placeholder="Enter new Task" onKeyDown={this.handleKeyDown}/>
       <button onClick = {this.handleClick} id="submitButton" type="submit" class="btn btn-primary">Submit</button>
       <div class = "todos">
-      <p> Your List Of Things To Do: {this.state.toDo.map(todo => <li>{todo}</li>)}
+      <p> Your List Of Things To Do: {this.state.toDo.map(todo => <li>{todo} <button value = {todo} onClick = {this.handleCheck}> X </button> </li>)}
       </p>
       </div>
       </div>
